6826725e27f13960b5f4bf73ffe9c5b1112f7d2d
Nils H <44339207+NilsHellwig@users.noreply.github.com>
2020-09-11 20:11:16 +0200

    bug fixes evaluation

--- evaluate.py
+++ evaluate.py
@@ -54,9 +54,10 @@ def get_time_info(query, datetime_relative):
 with open('evaluation.json', encoding='utf-8') as json_file:
     data = json.load(json_file)
 
-data = data["queries"]
+labeled_queries = data["queries"]
+
 # Statistics
-data_size = len(data)
+amount_of_labeled_queries = len(labeled_queries)
 data_with_valid_questions = 0
 correct_question_type = 0
 correct_city = 0
@@ -64,46 +65,48 @@ correct_time_type = 0
 correct_time = 0
 main_score = 0
 
-for query in data:
+for labeled_query in labeled_queries:
     print("-----------------------------------------------------------------------------")
-    query_text = query["text"]
+    query_text = labeled_query["text"]
     found_question_type = get_question_type(query_text)
-    #    print(found_question_type, query["question_type"])
-    if query["question_type"] != "None" or found_question_type != None:
+    found_city = cd.find_location_in_query(query_text)
+    if labeled_query["question_type"] != "None":
+        #The indication of the time/place is only evaluated if a valid question has been identified and a valid question is actually present.
+        # This has to do with the fact that the Assistant will recognize beforehand that the question is not valid.
+        # The Assistant will then also not bother to find a city/time in the query.
         data_with_valid_questions += 1
-        found_city = cd.find_location_in_query(query_text)
         found_time = td.get_formatted_time(query_text)
         found_time_type = found_time[0]
         found_time = found_time[1]
         found_when_question = bool(re.search("wann|zeitpunkt", query_text, re.IGNORECASE))
-        if found_city == query["city"]:
+        if found_city == labeled_query["city"]:
             correct_city += 1
             found_city_bool = True
         else:
-            if query["city"] == "None" and found_city is None:
+            if labeled_query["city"] == "None" and found_city is None:
                 correct_city += 1
                 found_city_bool = True
             else:
                 found_city_bool = False
-        if found_question_type == query["question_type"] and found_when_question == query["when_question"]:
+        if found_question_type == labeled_query["question_type"] and found_when_question == labeled_query["when_question"]:
             found_question_type_bool = True
             correct_question_type += 1
         else:
             found_question_type_bool = False
-        datetime_relative_to = datetime.datetime.strptime(query["timeinfo"], "%Y.%m.%d %H:%M")
+        datetime_relative_to = datetime.datetime.strptime(labeled_query["timeinfo"], "%Y.%m.%d %H:%M")
         time_result = get_time_info(query_text, datetime_relative_to)
 
         found_time_type_bool = False
         try:
-            if time_result["type"] == query["time"]["time_type"]:
+            if time_result["type"] == labeled_query["time"]["time_type"]:
                 found_time_type_bool = True
-            if time_result["type"] == "time_point" and query["time"]["time_type"] == "time_point":
+            if time_result["type"] == "time_point" and labeled_query["time"]["time_type"] == "time_point":
                 correct_time_type += 1
 
                 extracted_date = time_result["extracted_date_datetime"]
                 extracted_time = time_result["extracted_time_datetime"]
 
-                ground_truth = datetime.datetime.strptime(query["time"]["time_objects"]["start"], "%Y.%m.%d %H:%M")
+                ground_truth = datetime.datetime.strptime(labeled_query["time"]["time_objects"]["start"], "%Y.%m.%d %H:%M")
                 ground_truth_date = ground_truth.date()
                 ground_truth_time = ground_truth.time()
 
@@ -112,10 +115,10 @@ for query in data:
                     time_bool = True
                 else:
                     time_bool = False
-            if time_result["type"] == "day" and query["time"]["time_type"] == "day":
+            if time_result["type"] == "day" and labeled_query["time"]["time_type"] == "day":
                 correct_time_type += 1
                 extracted_date = time_result["extracted_date_datetime"]
-                ground_truth_date = datetime.datetime.strptime(query["time"]["time_objects"]["start"],
+                ground_truth_date = datetime.datetime.strptime(labeled_query["time"]["time_objects"]["start"],
                                                                "%Y.%m.%d %H:%M").date()
                 if extracted_date == ground_truth_date:
                     correct_time += 1
@@ -123,16 +126,16 @@ for query in data:
                 else:
                     time_bool = False
 
-            if time_result["type"] == "range" and query["time"]["time_type"] == "range":
+            if time_result["type"] == "range" and labeled_query["time"]["time_type"] == "range":
                 correct_time_type += 1
                 range_start = found_time[0]
                 range_end = found_time[1]
                 extracted_date_start = time_result["extracted_range_duration_start_datetime"].date()
                 extracted_date_end = time_result["extracted_range_duration_end_datetime"].date()
-                ground_truth_start = datetime.datetime.strptime(query["time"]["time_objects"]["start"],
+                ground_truth_start = datetime.datetime.strptime(labeled_query["time"]["time_objects"]["start"],
                                                                 "%Y.%m.%d %H:%M")
                 ground_truth_start_date = ground_truth_start.date()
-                ground_truth_end = datetime.datetime.strptime(query["time"]["time_objects"]["end"], "%Y.%m.%d %H:%M")
+                ground_truth_end = datetime.datetime.strptime(labeled_query["time"]["time_objects"]["end"], "%Y.%m.%d %H:%M")
                 ground_truth_end_date = ground_truth_end.date()
                 if ground_truth_start_date == extracted_date_start and ground_truth_end_date == extracted_date_end:
                     correct_time += 1
@@ -150,21 +153,17 @@ for query in data:
             correct_interpretation_of_query = False
         print("| Query: ", query_text, "| question type: ", str(found_question_type_bool), "| city: ", str(found_city_bool), "| time type: ", str(found_time_type_bool), "| time: ", str(time_bool), "| correct interpretation of query: ", str(correct_interpretation_of_query), "|")
     else:
-        if found_question_type is None and query["question_type"] == "None":
+        if found_question_type is None and labeled_query["question_type"] == "None":
             print("| Query: ", query_text, "|", "Korrekterweise wurde kein Fragetyp gefunden.")
             main_score += 1
             correct_question_type += 1
-        if found_question_type is None and query["question_type"] != "None":
-
-            print("| Query: ", query_text, "|", "Es wurde ein Fragetyp gefunden, obwohl die Query sinnlos ist.")
-        if found_question_type is None and query["question_type"] != "None":
-            print("| Query: ", query_text, "|",
-                  "Es wurde kein Fragetyp gefunden, obwohl die Query eine valide Frage ist.")
+        if found_question_type != None and labeled_query["question_type"] == "None":
+            print("| Query: ", query_text, "|","Es wurde ein Fragetyp gefunden, obwohl die Query sinnlos ist.")
 
 print("#---------------------------------------------------------------------------#")
-print("| Correct Question Types: ", correct_question_type / data_size)
+print("| Correct Question Types: ", correct_question_type / amount_of_labeled_queries)
 print("| Correct City: ", correct_city / data_with_valid_questions)
 print("| Correct Time Type: ", correct_time_type / data_with_valid_questions)
 print("| Correct Time: ", correct_time / data_with_valid_questions)
 print("#---------------------------------------------------------------------------#")
-print("| Main Accuracy: ", main_score / data_size)
+print("| Main Accuracy: ", main_score / amount_of_labeled_queries)
